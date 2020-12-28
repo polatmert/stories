9e32360985d4c20a5634b2c6c9b03cf8eceda334
Hüsnü Mert POLAT <husnumertpolatt@gmail.com>
2020-12-28 16:36:15 +0300

    Custom validator added and display name defect resolved

--- ws/src/main/java/com/stories/ws/user/User.java
+++ ws/src/main/java/com/stories/ws/user/User.java
@@ -1,9 +1,12 @@
 package com.stories.ws.user;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
  
@@ -16,11 +19,18 @@ public class User {
 	private long id;
 	
 	@NotNull
+	@Size(min=4, max=64)
+	//@Column(unique = true)
+	@UniqueUsername
 	private String username;
 	
 	@NotNull
-	private String displayname;
+	@Size(min=4, max=64)
+	private String displayName;
 	
+	@NotNull
+	@Size(min=8, max=64)
+	//@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$")
 	private String password;
 	
 	
